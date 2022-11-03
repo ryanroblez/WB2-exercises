1f5520d9b6e8c85c0db3bdb1be02bbab0f6d820d
ryanroblez <raroblez@gmail.com>
2022-11-03 14:09:36 -0700

    Numeric Conversions

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,29 @@
+// Description: This script has various numeric conversion techniques
+// Author: Jordan Q. Newprogrammer
+
+// Inputs
+let a = "101.1";
+let b = "55";
+let c = "402 Stevens";
+let d = "Number 5";
+
+// Calculations
+console.log(parseInt(a));
+console.log(parseInt(b));
+console.log(parseInt(c));
+console.log(parseInt(d));
+
+console.log(parseFloat(a));
+console.log(parseFloat(b));
+console.log(parseFloat(c));
+console.log(parseFloat(d));
+
+console.log(Number(a));
+console.log(Number(b));
+console.log(Number(c));
+console.log(Number(d));
+
+console.log(+a);
+console.log(+b);
+console.log(+c);
+console.log(+d);
